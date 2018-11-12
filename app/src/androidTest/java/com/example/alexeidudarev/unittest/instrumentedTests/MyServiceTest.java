b1a56644b45f15c1a991492eb3af35b01843352e
alexeidudarev <alexei.dudarev@gmail.com>
2018-11-12 03:47:47 +0200

    MyService test class with custom rules added

--- app/src/androidTest/java/com/example/alexeidudarev/unittest/instrumentedTests/MyServiceTest.java
+++ app/src/androidTest/java/com/example/alexeidudarev/unittest/instrumentedTests/MyServiceTest.java
@@ -1,11 +1,36 @@
 package com.example.alexeidudarev.unittest.instrumentedTests;
 
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ServiceTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import com.example.alexeidudarev.unittest.MyService;
+
+import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.concurrent.TimeoutException;
+
+import static org.junit.Assert.assertNotNull;
 
 @RunWith(AndroidJUnit4.class)
 public class MyServiceTest {
-
+    @Rule
+    public ServiceTestRule serviceTestRule = new ServiceTestRule();
+    @Test
+    public void testService()throws TimeoutException {
+        serviceTestRule.startService(new Intent(InstrumentationRegistry.getContext(),
+                MyService.class));
+    }
+    @Test
+    public void testBoundsService() throws TimeoutException{
+        IBinder binder = serviceTestRule.bindService(new
+                Intent(InstrumentationRegistry.getTargetContext(),MyService.class));
+        MyService service = ((MyService.LocalBinder) binder).getMyservice();
+        assertNotNull("Bound service is null",service);
+    }
 }
 
